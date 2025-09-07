0b93ee050ae8a53fb2fa8c8236ecb94b92db43b5
Fabi PR <161591047+FabiPR7@users.noreply.github.com>
2025-09-07 12:11:33 +0200

    Variable de users agregado

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const usersRoutes = require('./User/user'); 
 
 app.use(express.json());
 app.use(cors({ origin: ['http://localhost:5173', 'https://tuapp.vercel.app'] }));
