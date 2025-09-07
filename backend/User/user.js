77cebb2c59b8e51adf3cdb082c9153d653986f14
Fabi PR <161591047+FabiPR7@users.noreply.github.com>
2025-09-07 12:14:13 +0200

    Cambiado try

--- backend/User/user.js
+++ backend/User/user.js
@@ -7,7 +7,7 @@ router.get('/users', async (req, res) => {
     const result = await pool.query('SELECT * FROM users');
     res.json(result.rows);
   } catch (err) {
-    res.status(500).json({ error: 'Error al consultar la base de datos' });
+    res.status(500).json({ error: `Error al consultar la base de datos ${err}` });
   }
 });
 
